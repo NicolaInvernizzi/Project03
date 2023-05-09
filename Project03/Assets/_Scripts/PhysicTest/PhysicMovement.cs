993b61623326597083c3e09cb69c73f66d07c976
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-05-09 16:24:28 +0200

    Draw movement zone.
    Added debug movement zone & polishing.

--- Project03/Assets/_Scripts/PhysicTest/PhysicMovement.cs
+++ Project03/Assets/_Scripts/PhysicTest/PhysicMovement.cs
@@ -1,10 +1,9 @@
 using UnityEditor;
 using UnityEngine;
 
-// Old system: adjust forward force in order to change velocity direction
-
 public class PhysicMovement : MonoBehaviour
 {
+#region Variables
     [Header("---------------Rotation---------------"), Space(5)]
     [SerializeField] Vector3 minRotationRange = new Vector3(30, 30, 60);
     [SerializeField] Vector3 maxRotationRange = new Vector3(30, 30, 10);
@@ -12,77 +11,77 @@ public class PhysicMovement : MonoBehaviour
     [SerializeField, Range(1f, 200f)] float rotationSpeedLeftRight = 15f;
 
     [Space(10), Header("---------------Velocity---------------"), Space(5)]
-    [SerializeField] Vector3 min_DirectionVelocity = new Vector3(6f, -1.7f, 0f);
-    [SerializeField] Vector3 max_DirectionVelocity = new Vector3(0.5f, -1f, 0f);
-    [SerializeField] Vector3 min_DirectionVelocity2 = new Vector3(6f, -1.7f, 0f);
-    [SerializeField] Vector3 max_DirectionVelocity2 = new Vector3(0.5f, -1f, 0f);
+    [SerializeField] Vector3 directionVelocity_up = new Vector3(6f, -1.7f, 0f);
+    [SerializeField] Vector3 directionVelocity_down = new Vector3(0.5f, -1f, 0f);
+    [SerializeField] Vector3 directionVelocity_left = new Vector3(6f, -4f, -2f);
+    [SerializeField] Vector3 directionVelocity_right = new Vector3(6f, -4f, 2f);
     [SerializeField, Range(0f, 200f)] float min_MagnitudeVelocity = 5f;
-    [SerializeField, Range(0f, 200f)] float max_MagnitudeVelocity = 10f;
-    [SerializeField] Color min_MagnitudeVelocity_Color = Color.black;
-    [SerializeField] Color max_MagnitudeVelocity_Color = Color.cyan;
+    [SerializeField, Range(0f, 200f)] float max_MagnitudeVelocity = 20f;
 
     [Space(10), Header("---------------Debug---------------"), Space(5)]
-    [SerializeField, Range(5f, 200f)] float lineLength = 10f;
     [SerializeField] Vector3 labelPosition = Vector3.one;
     [SerializeField] bool mouse_wasd;
     [SerializeField] bool stopPlayer;
     [SerializeField] Vector3 stopPosition;
-    [SerializeField] bool showVelocity;
-    [SerializeField] bool showUpDownLimits;
-    [SerializeField] bool showLeftRightLimits;
-    [SerializeField] bool upDown;
-    [SerializeField] bool leftRight;
-    [SerializeField] bool both;
+
+    [Space(10), Header("---------------Draw---------------"), Space(5)]
+    [SerializeField, Range(5f, 200f)] float lineLength = 10f;
+    [SerializeField] bool draw_Velocity = true;
+    [SerializeField] bool draw_DirectionUpDown;
+    [SerializeField] bool draw_DirectionLeftRight;
+    [SerializeField] bool draw_MovemenZone = true;
+    [SerializeField, Range(0.01f, 1f)] float frequencyLine = 0.01f;
+    /*
+      [SerializeField] bool draw_UpDown;
+      [SerializeField] bool draw_LeftRight;
+    */
 
     Rigidbody rb;
     Vector3 rotation;
     Vector3 startingRotation;
-    Vector3 directionVelocity;
-    Vector3 directionVelocity2;
-    Color colorMagnitudeVelocity;
-
-    // Old system: [SerializeField, Min(0f)] float minInclination_OverallSpeed = 2f;
-    // Old system: [SerializeField, Min(0f)] float maxInclination_OverallSpeed = 10f;
-    // Old system: [SerializeField, Min(0f)] float gravity = 1f;
-    // Old system: [SerializeField, Min(0f)] float moveForward = 1f;
-    // Old system: Vector3 move;
-
+    Vector3 directionVelocityUpDown;
+    Vector3 directionVelocityLeftRight;
     float magnitudeVelocity;
+    #endregion
+
+#region Flow
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         startingRotation = transform.eulerAngles;
         rotation = transform.eulerAngles;
-
-        // Old system: move.x = -gravity;
-        // Old system: move.y = moveForward;
-    }
-    void FixedUpdate()
-    {
-        Rotate();
-        Move();
+        stopPosition = transform.position;
     }
     void Update()
     {
-        if(showVelocity)
-            Debug.DrawRay(transform.position, rb.velocity * lineLength, colorMagnitudeVelocity);
+        if (draw_Velocity)
+            Debug.DrawRay(transform.position, rb.velocity * lineLength, Color.black);
 
-        if(showUpDownLimits)
+        if (draw_DirectionUpDown)
         {
-            Debug.DrawRay(transform.position, max_DirectionVelocity.normalized * max_MagnitudeVelocity * lineLength, Color.magenta);
-            Debug.DrawRay(transform.position, min_DirectionVelocity.normalized * min_MagnitudeVelocity * lineLength, Color.gray);
+            Debug.DrawRay(transform.position, directionVelocity_down.normalized * max_MagnitudeVelocity * lineLength, Color.magenta);
+            Debug.DrawRay(transform.position, directionVelocity_up.normalized * min_MagnitudeVelocity * lineLength, Color.gray);
         }
 
-        if(showLeftRightLimits)
+        if (draw_DirectionLeftRight)
         {
-            Debug.DrawRay(transform.position, max_DirectionVelocity2.normalized * max_MagnitudeVelocity * lineLength, Color.green);
-            Debug.DrawRay(transform.position, min_DirectionVelocity2.normalized * min_MagnitudeVelocity * lineLength, Color.blue);
+            Debug.DrawRay(transform.position, directionVelocity_left.normalized * rb.velocity.magnitude * lineLength, Color.green);
+            Debug.DrawRay(transform.position, directionVelocity_right.normalized * rb.velocity.magnitude * lineLength, Color.blue);
         }
 
-        // Old system: Debug.DrawRay(transform.position, transform.up * move.y * debug_vectorLength, Color.green);
-        // Old system: Debug.DrawRay(transform.position, Vector3.up * move.x * debug_vectorLength, Color.red);
-        // Old system: Debug.DrawRay(transform.position, Vector3.forward * move.z * debug_vectorLength, Color.blue);
+        if(draw_MovemenZone)
+            DrawSimulation();
     }
+    void FixedUpdate()
+    {
+        if(!stopPlayer)
+            stopPosition = transform.position;
+        Rotate();
+        Move();
+    }
+#endregion
+
+#region Methods
     void OnDrawGizmos()
     {
         if (rb != null)
@@ -124,9 +123,6 @@ public class PhysicMovement : MonoBehaviour
         rotation.y = Mathf.Clamp(rotation.y, startingRotation.y - minRotationRange.y, startingRotation.y + maxRotationRange.y);
         rotation.z = Mathf.Clamp(rotation.z, startingRotation.z - minRotationRange.z, startingRotation.z + maxRotationRange.z);
         transform.localRotation = Quaternion.Euler(rotation);
-
-        // Old system: move.y = Remap(startingRotation.z - minRotationRange.z, startingRotation.z + maxRotationRange.z,
-        //                           maxInclination_OverallSpeed, minInclination_OverallSpeed, rotation.z);
     }
     void Move()
     {
@@ -137,29 +133,101 @@ public class PhysicMovement : MonoBehaviour
             transform.position = stopPosition;
 
         // direction
-        directionVelocity = RemapSlerp(startingRotation.z - minRotationRange.z, startingRotation.z + maxRotationRange.z,
-                                max_DirectionVelocity, min_DirectionVelocity, rotation.z);
-        directionVelocity2 = RemapSlerp(startingRotation.x - minRotationRange.x, startingRotation.x + maxRotationRange.x,
-                                      max_DirectionVelocity2, min_DirectionVelocity2, rotation.x);
+        directionVelocityUpDown = RemapSlerp(startingRotation.z - minRotationRange.z, startingRotation.z + maxRotationRange.z,
+                                directionVelocity_down, directionVelocity_up, rotation.z);
+        directionVelocityLeftRight = RemapSlerp(startingRotation.x - minRotationRange.x, startingRotation.x + maxRotationRange.x,
+                                      directionVelocity_left, directionVelocity_right, rotation.x);
+        rb.velocity = (directionVelocityUpDown + directionVelocityLeftRight).normalized * magnitudeVelocity;
+    }
+    void DrawSimulation()
+    {
+        /*
+           if (draw_UpDown)
+            DrawComplex(false, false, false, true, true, false, false);
+           if (draw_LeftRight)
+            DrawComplex(true, true, false, false, false, false, false);
+        */
 
-        // set velocity
-        if (leftRight)
-            rb.velocity = directionVelocity2.normalized * magnitudeVelocity;
+        DrawComplex(true, false, true, true, true, false, false);
+        DrawComplex(true, false, false, true, true, false, false);
 
-        if(upDown)
-            rb.velocity = directionVelocity.normalized * magnitudeVelocity;
-  
-        if (both) // direction + direction2 -> circumference with y lerped ymin, ymax and x lerped xmin, xmax.
-            rb.velocity = (directionVelocity+ directionVelocity2).normalized * magnitudeVelocity;
+        DrawComplex(true, true, false, true, false, true, false);
+        DrawComplex(true, true, false, true, false, false, false);
 
-        // velocity color adjustment
-        colorMagnitudeVelocity = RemapColor(min_MagnitudeVelocity, max_MagnitudeVelocity, 
-                                            min_MagnitudeVelocity_Color, max_MagnitudeVelocity_Color, rb.velocity.magnitude);
+        DrawComplex(true, true, false, true, true, false, false);
+        DrawComplex(true, true, false, true, true, false, true);
 
-        // Old system: rb.velocity = (transform.up * move.y + Vector3.up * move.x + Vector3.forward * move.z).normalized *
-        //              Remap(startingRotation.z - minRotationRange.z, startingRotation.z + maxRotationRange.z,
-        //                     max_MagnitudeVelocity, min_MagnitudeVelocity, rotation.z);
+        Debug.DrawLine(transform.position, transform.position + (directionVelocity_down + directionVelocity_right).normalized * max_MagnitudeVelocity * lineLength);
+        Debug.DrawLine(transform.position, transform.position + (directionVelocity_down + directionVelocity_left).normalized * max_MagnitudeVelocity * lineLength);
+        Debug.DrawLine(transform.position, transform.position + (directionVelocity_up + directionVelocity_right).normalized * min_MagnitudeVelocity * lineLength);
+        Debug.DrawLine(transform.position, transform.position + (directionVelocity_up + directionVelocity_left).normalized * min_MagnitudeVelocity * lineLength);
+    }
+    void DrawComplex(bool drawLR, bool moveLR, bool maxLR, bool drawUD, bool moveUD, bool maxUD, bool invert)
+    {
+        Vector3 direction1 = Vector3.zero;
+        Vector3 new_direction1 = Vector3.zero;
+        Vector3 direction2 = Vector3.zero;
+        Vector3 new_direction2 = Vector3.zero;
+        float magnitude = 0f;
+        float new_magnitude = 0f;
+        float k1 = 0f;
+        float k2 = 0f;
+        float h1 = 0f;
+        float h2 = 0f;
 
+        for (float i = 0; i < 1; i += frequencyLine)
+        {
+            if (drawLR)
+            {
+                // down -> up
+                if (moveLR)
+                {
+                    h1 = i;
+                    h2 = frequencyLine;
+                }
+                else
+                {
+                    if (maxLR)
+                        h1 = 1;
+                }
+
+                if (invert)
+                {
+                    direction2 = RemapSlerp(1, 0, directionVelocity_left, directionVelocity_right, h1);
+                    new_direction2 = RemapSlerp(1, 0, directionVelocity_left, directionVelocity_right, h1 + h2);
+                }
+                else
+                {
+                    direction2 = RemapSlerp(0, 1, directionVelocity_left, directionVelocity_right, h1);
+                    new_direction2 = RemapSlerp(0, 1, directionVelocity_left, directionVelocity_right, h1 + h2);
+                }
+                magnitude = rb.velocity.magnitude;
+                new_magnitude = rb.velocity.magnitude;
+            }
+
+            if (drawUD)
+            {
+                // left -> right
+                if (moveUD)
+                {
+                    k1 = i;
+                    k2 = frequencyLine;
+                }
+                else
+                {
+                    if (maxUD)
+                        k1 = 1;
+                }
+
+                direction1 = RemapSlerp(0, 1, directionVelocity_down, directionVelocity_up, k1);
+                new_direction1 = RemapSlerp(0, 1, directionVelocity_down, directionVelocity_up, k1 + k2);
+                magnitude = Remap(0, 1, max_MagnitudeVelocity, min_MagnitudeVelocity, k1);
+                new_magnitude = Remap(0, 1, max_MagnitudeVelocity, min_MagnitudeVelocity, k1 + k2);
+            }
+
+            Debug.DrawLine(transform.position + (direction1 + direction2).normalized * magnitude * lineLength,
+                           transform.position + (new_direction1 + new_direction2).normalized * new_magnitude * lineLength);
+        }
     }
     public static float Remap(float min_A, float max_A, float min_B, float max_B, float A)
     {
@@ -171,9 +239,10 @@ public class PhysicMovement : MonoBehaviour
         float t = Mathf.InverseLerp(min_A, max_A, A);
         return Vector3.Slerp(min_B, max_B, t);
     }
-    public static Color RemapColor(float min_A, float max_A, Color min_B, Color max_B, float A)
+    public static Vector3 RemapLerp(float min_A, float max_A, Vector3 min_B, Vector3 max_B, float A)
     {
         float t = Mathf.InverseLerp(min_A, max_A, A);
-        return Color.Lerp(min_B, max_B, t);
+        return Vector3.Lerp(min_B, max_B, t);
     }
+#endregion
 }
